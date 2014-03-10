c3819f6c9458d5340ed0887323962f21641eb620
t.hayashi <t.hayashi0077+bitbucket@gmail.com>
2014-03-10 22:36:38 +0900

    Initial Commit

---
+++ warwolfdb-demo/WLFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WLFAppDelegate.h
+//  warwolfdb-demo
+//
+//  Created by 林 達也 on 2014/03/10.
+//  Copyright (c) 2014年 林 達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ warwolfdb-demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  warwolfdb-demo
+//
+//  Created by 林 達也 on 2014/03/10.
+//  Copyright (c) 2014年 林 達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WLFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WLFAppDelegate class]));
+    }
+}
